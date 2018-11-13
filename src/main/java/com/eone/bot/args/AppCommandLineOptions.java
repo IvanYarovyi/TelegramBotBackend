5e766fa6159f5b9e20a19112a1506a12a09fe16d
IvanYarovyi <vanya.yarovoy@gmail.com>
2018-11-13 23:02:43 +0200

    CL help was added

--- src/main/java/com/eone/bot/args/AppCommandLineOptions.java
+++ src/main/java/com/eone/bot/args/AppCommandLineOptions.java
@@ -15,7 +15,12 @@ public class AppCommandLineOptions {
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
-            formatter.printHelp("telegram-bot-backend", options);
+            formatter.printHelp(100,
+                    " bot-backend-all.jar OPTIONS...",
+                    "only option -c is mandatory",
+                    options,
+                    "example: java -jar bot-backend-all.jar -t=123456789:AAA-sssss_D8oXaqBBEoaaaafQhdsddsdql -p=8082");
+
             System.exit(1);
         }
     }
@@ -25,15 +30,15 @@ public class AppCommandLineOptions {
         opt1.setRequired(true);
         options.addOption(opt1);
 
-        Option opt2 = new Option("c", OPTION.CERTIFICATE.toString(), true, "Path to certificate");
+        Option opt2 = new Option("c", OPTION.CERTIFICATE.toString(), true, "Path to certificate. Certificate can either be verified or self-signed");
         opt2.setRequired(false);
         options.addOption(opt2);
 
-        Option opt3 = new Option("h", OPTION.WEB_HOOK.toString(), true, "Web hook url");
+        Option opt3 = new Option("h", OPTION.WEB_HOOK.toString(), true, "Webhook url for Telegram (public IP)");
         opt3.setRequired(false);
         options.addOption(opt3);
 
-        Option opt4 = new Option("p", OPTION.SERVER_PORT.toString(), true, "Web server port");
+        Option opt4 = new Option("p", OPTION.SERVER_PORT.toString(), true, "Web server port for this application");
         opt4.setRequired(false);
         options.addOption(opt4);
     }
