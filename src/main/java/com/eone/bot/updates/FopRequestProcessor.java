bb4abe74272735f07a5a9ef29f133f77964b3c2a
Eone <githubVano8002>
2018-12-25 23:52:02 +0200

    Basic Latin encoding, test were added

--- src/main/java/com/eone/bot/updates/FopRequestProcessor.java
+++ src/main/java/com/eone/bot/updates/FopRequestProcessor.java
@@ -15,8 +15,16 @@ import java.util.List;
 public class FopRequestProcessor implements UpdateProcessor {
 
     private static final org.apache.logging.log4j.Logger LOG = LogManager.getLogger(FopRequestProcessor.class);
+
     //"За запитом: '%s' в базі нічого не знайденно."
-    public static final String NO_DATA_MESSAGE = "\u0417\u0430 \u0437\u0430\u043f\u0438\u0442\u043e\u043c: '%s' \u0432 \u0431\u0430\u0437\u0456 \u043d\u0456\u0447\u043e\u0433\u043e \u043d\u0435 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u043d\u043e.";
+    private static final String MESSAGE_NO_DATA = "\u0417\u0430 \u0437\u0430\u043f\u0438\u0442\u043e\u043c: %s \u0432 \u0431\u0430\u0437\u0456 \u043d\u0456\u0447\u043e\u0433\u043e \u043d\u0435 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u043d\u043e.";
+    //"Не валідний запит. Спробуйте: _Мельник Іван Іванович_ або _Мельник Іван_ ";
+    private static final String MESSAGE_QUERY_EXAMPLE = "\u041d\u0435 \u0432\u0430\u043b\u0456\u0434\u043d\u0438\u0439 \u0437\u0430\u043f\u0438\u0442. \u0421\u043f\u0440\u043e\u0431\u0443\u0439\u0442\u0435: _\u041c\u0435\u043b\u044c\u043d\u0438\u043a \u0406\u0432\u0430\u043d \u0406\u0432\u0430\u043d\u043e\u0432\u0438\u0447_ \u0430\u0431\u043e _\u041c\u0435\u043b\u044c\u043d\u0438\u043a \u0406\u0432\u0430\u043d_ ";
+    //"За фільтром %s в базі знайденно: %d записів."
+    private static final String MESSAGE_FOUND_COUNT = "\u0417\u0430 \u0444\u0456\u043b\u044c\u0442\u0440\u043e\u043c %s \u0432 \u0431\u0430\u0437\u0456 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u043d\u043e: %d \u0437\u0430\u043f\u0438\u0441\u0456\u0432.";
+    //"\n _Список прізвищ що співпали з фільтром:_"
+    private static final String MESSAGE_LIST = "\n _\u0421\u043f\u0438\u0441\u043e\u043a \u043f\u0440\u0456\u0437\u0432\u0438\u0449 \u0449\u043e \u0441\u043f\u0456\u0432\u043f\u0430\u043b\u0438 \u0437 \u0444\u0456\u043b\u044c\u0442\u0440\u043e\u043c:_";
+
 
     private TelegramBot telegramBot;
     private FopNormDao fopDao;
@@ -26,27 +34,32 @@ public class FopRequestProcessor implements UpdateProcessor {
         this.fopDao = fopDao;
     }
 
+    private void sendMessage(Long chatId, String message) {
+        SendMessage request = new SendMessage(chatId, message);
+        request.parseMode(ParseMode.Markdown);
+        SendResponse telegtamResponse = telegramBot.execute(request);
+        LOG.trace(telegtamResponse);
+    }
+
     @Override
     public boolean processUpdate(Update update) {
         String userQuery = update.message().text();
+        Long chatId = update.message().chat().id();
 
-        String[] words = getQueryParams(userQuery);
+        String[] words = getWordsFromText(userQuery);
         if(!valid(words)){
-            SendMessage response = new SendMessage(update.message().chat().id(), "Not valid query, try Іван Іванов or Ivan Ivanov Petrovych");
-            response.parseMode(ParseMode.HTML);
-            SendResponse execute = telegramBot.execute(response);
-            LOG.debug(execute.message());
+            sendMessage(chatId, MESSAGE_QUERY_EXAMPLE);
+            return false;
         }
         if (words.length == 2){
-            return getFopNames(update, userQuery, words);
+            return getFopNames(chatId, userQuery, words);
         } else if (words.length == 3){
-            return sendFopsAllInfoList(update, words);
+            return sendFopsAllInfoList(chatId, words);
         }
         return true;
     }
 
-    private boolean getFopNames(Update update, String userQuery, String[] words) {
-        Long id = update.message().chat().id();
+    private boolean getFopNames(Long chatId, String userQuery, String[] words) {
         List<FopNorm> fops = null;
         try {
             String[] s = convertToFullTextSearch(words);
@@ -57,29 +70,21 @@ public class FopRequestProcessor implements UpdateProcessor {
         }
         //todo make fop limit, grouping, count ...
         if (fops.size() == 0) {
-            SendMessage response = new SendMessage(update.message().chat().id(), String.format(NO_DATA_MESSAGE, userQuery));
-            response.parseMode(ParseMode.HTML);
-            SendResponse execute = telegramBot.execute(response);
-            LOG.trace(execute);
+            sendMessage(chatId, String.format(MESSAGE_NO_DATA, userQuery));
         } else {
             StringBuilder sb = new StringBuilder();
-            sb.append(String.format("За фільтром %s в базі знайденно: %d записів.", userQuery, fops.size()));
-            sb.append("\n *Уточніть ваш запит, та спробуйте знову.*");
-            sb.append("\n _Список прізвищ що співпали з фільтром:_");
+            sb.append(String.format(MESSAGE_FOUND_COUNT, userQuery, fops.size()));
+            sb.append(MESSAGE_LIST);
             fops.stream()
                     .forEach(fop -> sb.append(fop.getOther_name())
                     .append("\n"));
-            SendMessage response
-                    = new SendMessage(update.message().chat().id(), sb.toString());
-            response.parseMode(ParseMode.Markdown);
-            SendResponse execute = telegramBot.execute(response);
-            LOG.trace(execute);
+            sendMessage(chatId, sb.toString());
             return true;
         }
         return false;
     }
 
-    private boolean sendFopsAllInfoList(Update update, String[] words) {
+    private boolean sendFopsAllInfoList(Long chatId, String[] words) {
         List<FopNorm> fops = null;
         try {
             String[] s = convertToFullTextSearch(words);
@@ -88,32 +93,26 @@ public class FopRequestProcessor implements UpdateProcessor {
             LOG.warn(e.getMessage());
             return true;
         }
-        //todo make fop limit, grouping, count ...
+
         if (fops.size() == 0) {
-            SendMessage response = new SendMessage(update.message().chat().id(), NO_DATA_MESSAGE);
-            response.parseMode(ParseMode.HTML);
-            SendResponse execute = telegramBot.execute(response);
-            LOG.trace(execute);
+            sendMessage(chatId, MESSAGE_NO_DATA);
         } else {
             for (FopNorm fop : fops) {
-                SendMessage response = new SendMessage(update.message().chat().id(), formatOutput(fop));
-                response.parseMode(ParseMode.Markdown);
-                SendResponse execute = telegramBot.execute(response);
-                LOG.trace(execute);
+                sendMessage(chatId, formatOutput(fop));
             }
         }
         return false;
     }
 
-    public static boolean valid(String[] words){
+    private static boolean valid(String[] words){
         if (words.length == 2 || words.length == 3){
             return true;
         }
         return false;
     }
 
-    private static String[] getQueryParams(String query) {
-        String[] words = query.split("\\s+");
+    private static String[] getWordsFromText(String userText) {
+        String[] words = userText.split("\\s+");
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].replaceAll("^\\w", "");
         }
---
+++ src/test/java/FopRequestProcessorTest.java
@@ -0,0 +1,14 @@
+import com.eone.bot.updates.FopRequestProcessor;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class FopRequestProcessorTest {
+
+    @Test
+    public void validate() {
+        new FopRequestProcessorTest();
+    }
+}
